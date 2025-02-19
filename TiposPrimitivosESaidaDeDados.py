29003e245a1916a0fe62d18e567f2c3e8fd6f299
0mariapaula <0mariapaulaferreira@gmail.com>
2025-02-19 20:43:59 -0300

    TiposPrimitivosESaidaDeDados 0.2

--- TiposPrimitivosESaidaDeDados.py
+++ TiposPrimitivosESaidaDeDados.py
@@ -1,10 +1,15 @@
-n1 = int(input("digite um numero"))
-n2 = int(input("digite um numero"))
-s = n1+n2
-print ("a soma" + s)
+#n1 = int(input("digite um numero"))
+#n2 = int(input("digite um numero"))
+#s = n1+n2
+#print ("a soma" + s) 
+#print ("soma vale()".format(s)) # essa sintaxe e a nova 
 
 #int = 7, -7,0
 #floot = 2.5 , 69.2223 , -123.345, 7.0
 #bool = true, false
 #str = "ola pessoas do meu ig!!! turu baum ?"
 
+nome = str(input("Ola! informe o seu nome:  "))
+ano = int(input("Agora " + nome + " informe o ano que nasceu: "))
+x = 2025 - ano
+print("Oi {}, a sua idade é {}".format(nome, x))
