e8adba0699ff5ce149694b3440e5fbf5e4a30290
0mariapaula <0mariapaulaferreira@gmail.com>
2025-04-28 20:26:12 -0300

    if else

---
+++ Condiçoes /condicoes01.py
@@ -0,0 +1,9 @@
+n1 = int(input("Digite um numero: "))
+n2 = int(input("Digite outro numero: "))
+
+r = n1+n2
+
+if (r >= True):
+    print ("Numero {} é positivo: " .format(r))
+else:
+    print("Numero {} é negativo".format(r))
